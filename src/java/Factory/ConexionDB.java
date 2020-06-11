79257baf0ee519d945ae7df71290b7e0768d6d0d
palaciosky <63530913+palaciosky@users.noreply.github.com>
2020-06-10 21:47:37 -0600

    empieza crear archivos par la conexion de base de datos

---
+++ src/java/Factory/ConexionDB.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Palacios
+ */
+public abstract class ConexionDB {
+    protected String[] parametros;//Arreglo para recibir toda la conexion
+    protected Connection conexion;
+    //El siguiente metodo abstracto no se implementa solo se declara
+    //implementara en la subclase
+    abstract Connection open();//Method abstracto que devuelve un objeto connetion
+
+    public ResultSet consultaSQL (String consulta){
+   Statement st;
+   ResultSet rs = null;
+   try{
+     st = conexion.createStatement();
+     rs = st.executeQuery(consulta);//pa ejecutar la consulta
+    }catch (SQLException ex){
+    ex.printStackTrace();
+    }
+   return rs;
+  }
+ 
+  public boolean cerrarConexion(){
+  boolean ok = true;
+  try{
+  conexion.close();
+  }catch (Exception es){
+   ok = false;
+   es.printStackTrace();
+  }
+  return ok;
+  }
+}
