b58a8e6cd558d422cbd2cec0770d1d4a6e2cb0fc
palaciosky <63530913+palaciosky@users.noreply.github.com>
2020-06-20 20:41:09 -0600

    imprimir pdf parcialmente funcional en test

---
+++ src/java/Print/pdf.java
@@ -0,0 +1,108 @@
+
+package Print;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Palacios
+ */
+@WebServlet(name = "pdf", urlPatterns = {"/pdf"})
+public class pdf extends HttpServlet {
+    private Object Chunck;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/pdf");
+        OutputStream out = response.getOutputStream();
+        try  {
+            try{
+                Document documento = new Document();
+                PdfWriter.getInstance(documento, out);
+                
+                documento.open();
+                
+                Paragraph par1 = new Paragraph();
+                Font ftitulo = new Font(Font.FontFamily.HELVETICA,16,Font.BOLD,BaseColor.BLACK);
+                par1.add(new Phrase("Lista de Categorias",ftitulo));
+                par1.setAlignment(Element.ALIGN_CENTER);
+                par1.add(new Phrase(Chunk.NEWLINE));
+                par1.add(new Phrase(Chunk.NEWLINE));
+                documento.add(par1);
+                
+                Paragraph par2 = new Paragraph();
+                Font fdesc = new Font(Font.FontFamily.HELVETICA,12,Font.NORMAL,BaseColor.BLACK);
+                par2.add(new Phrase("Categorias segun su estado",fdesc));
+                par2.setAlignment(Element.ALIGN_JUSTIFIED);
+                par2.add(new Phrase(Chunk.NEWLINE));
+                par2.add(new Phrase(Chunk.NEWLINE));
+                documento.add(par2);
+                
+                documento.close();
+            }catch(DocumentException ex){
+            ex.getMessage();
+            }
+           
+        } finally{
+        out.close();
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/Vistas-Categorias/listarCategorias.jsp
+++ web/Vistas-Categorias/listarCategorias.jsp
@@ -22,6 +22,7 @@
         <div style="width: 600px;" >
             <a href="#" class="btn btn-success btn-sm glyphicon glyphicon-pencil" role="button"  >Agregar Categoria +</a>
             <a onclick="javascript:window.imprimirDIV('ID_DIV');" href="#"  class="btn btn-success btn-sm " role="button" ><span class="glyphicon glyphicon-print"> PDF</span> </a>
+             <a href="pdf" class="btn btn-success btn-sm glyphicon glyphicon-pencil" role="button"  >Agregar Categoria +</a>
             <h3>Listado de categorias Registradas</h3>
              <script>
                 function imprimirDIV(ID_DIV) {
